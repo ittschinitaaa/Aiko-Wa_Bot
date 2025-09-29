ca689f6304297791bf97ce3976eda8f5bc5f8f25
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-29 01:36:05 -0300

    Update group-admins.js

--- plugins/group-admins.js
+++ plugins/group-admins.js
@@ -17,8 +17,7 @@ conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, {mentions: [...groupAdmin
 
 handler.help = ['admins']
 handler.tags = ['grupo']
-handler.customPrefix = /a|@/i
-handler.command = /^(admins|@admins|dmins)$/i
+handler.command = ['admins', 'admin']
 handler.group = true
 
 export default handler
