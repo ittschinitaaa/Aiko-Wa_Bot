9c66ef1b15f48fbc3083477cc4ad38dd0b75b6bf
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-29 01:37:44 -0300

    Update group-admins.js

--- plugins/group-admins.js
+++ plugins/group-admins.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn, participants, groupMetadata, args}) => {
 const primaryBot = global.db.data.chats[m.chat].primaryBot
 if (primaryBot && conn.user.jid !== primaryBot) throw !1
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => 'https://files.catbox.moe/xr2m6u.jpg')
+const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => 'https://files.catbox.moe/rq6lzs.jpg')
 const groupAdmins = participants.filter((p) => p.admin)
 const listAdmin = groupAdmins.map(v => `● @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
