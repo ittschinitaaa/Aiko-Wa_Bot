6c7da0c6355efc45251ac6eceea00a37d4516871
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-28 22:47:41 -0300

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -3,7 +3,7 @@ const pesan = args.join` `
 const oi = `*» INFO :* ${pesan}`
 let teks = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 🗣️\n\n ${oi}\n\n╭  ┄ 𝅄 ۪꒰ \`⡞᪲=͟͟͞${botname}≼᳞ׄ\` ꒱ ۟ 𝅄 ┄\n`
 for (const mem of participants) {
-teks += `┊ꕥ @${mem.id.split('@')[0]}\n`
+teks += ` ᮫໋۟۟۟۟۟⌯ ᤳ݄፞★꯭݃ @${mem.id.split('@')[0]}\n`
 }
 teks += `╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ *${vs}* ୧ ׅ ꒱  ┄  ─ ┄⸼`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map((a) => a.id) })
