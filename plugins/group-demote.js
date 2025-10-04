14d4c75aec26f09219a3af07d6d52daa88358ee2
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-10-03 23:02:39 -0300

    Update group-demote.js

--- plugins/group-demote.js
+++ plugins/group-demote.js
@@ -1,31 +1,28 @@
 var handler = async (m, { conn, usedPrefix, command, text, groupMetadata, isAdmin }) => {
-  let mentionedJid = await m.mentionedJid
-  let user = mentionedJid && mentionedJid.length 
-      ? mentionedJid[0] 
-      : m.quoted && await m.quoted.sender 
-        ? await m.quoted.sender 
-        : null
+let mentionedJid = await m.mentionedJid
+let user = mentionedJid && mentionedJid.length ? mentionedJid[0] : m.quoted && await m.quoted.sender ? await m.quoted.sender : null
+if (!user) return conn.reply(m.chat, `❀ Debes mencionar a un usuario para poder quitarle los privilegios de administrador.`, m)
 
-  if (!user) return conn.reply(m.chat, `❀ Debes mencionar a un usuario para poder quitarle los privilegios de administrador.`, m)
+try {
+const groupInfo = await conn.groupMetadata(m.chat)
+const ownerGroup = groupInfo.owner || m.chat.split('-')[0] + '@s.whatsapp.net'
 
-  try {
-    const groupInfo = await conn.groupMetadata(m.chat)
-    const ownerGroup = groupInfo.owner || m.chat.split('-')[0] + '@s.whatsapp.net'
+// Verifica si es dueño del grupo
+if (user === ownerGroup) 
+return conn.reply(m.chat, 'ꕥ No se puede quitar el rol de administrador al dueño del grupo.', m)
 
-    if (user === ownerGroup) 
-      return conn.reply(m.chat, 'ꕥ No puedes quitarle el admin al propietario del grupo.', m)
+// Verifica si el usuario ya no es admin
+if (!groupInfo.participants.some(p => p.id === user && p.admin))
+return conn.reply(m.chat, 'ꕥ El usuario mencionado ya es miembro normal.', m)
 
-    let participante = groupInfo.participants.find(p => p.id === user)
-    if (!participante || !participante.admin) 
-      return conn.reply(m.chat, 'ꕥ El usuario mencionado no es administrador.', m)
+// Ejecuta el demote
+await conn.groupParticipantsUpdate(m.chat, [user], 'demote')
+await m.react('✅')
+//await conn.reply(m.chat, `❀ Se le quitó el admin correctamente.`, m)
 
-    await conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-    await m.react('✅')   
-    //await conn.reply(m.chat, `❀ Se le quitó el admin al usuario con éxito.`, m)
-  } catch (e) {
-    conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${e.message}`, m)
-  }
-}
+} catch (e) {
+conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${e.message}`, m)
+}}
 
 handler.help = ['demote']
 handler.tags = ['grupo']
