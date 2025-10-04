4f487380015b8f304c6f8685ec7d5f708b62a8f9
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-10-03 23:06:05 -0300

    Update group-demote.js

--- plugins/group-demote.js
+++ plugins/group-demote.js
@@ -1,32 +1,23 @@
-var handler = async (m, { conn, usedPrefix, command, text, groupMetadata, isAdmin }) => {
+var handler = async (m, { conn, usedPrefix, command, text, groupMetadata }) => {
 let mentionedJid = await m.mentionedJid
 let user = mentionedJid && mentionedJid.length ? mentionedJid[0] : m.quoted && await m.quoted.sender ? await m.quoted.sender : null
-if (!user) return conn.reply(m.chat, `❀ Debes mencionar a un usuario para poder quitarle los privilegios de administrador.`, m)
-
+if (!user) return conn.reply(m.chat, `❀ Debes mencionar a un usuario para poder promoverlo a administrador.`, m)
 try {
 const groupInfo = await conn.groupMetadata(m.chat)
-const ownerGroup = groupInfo.owner || m.chat.split('-')[0] + '@s.whatsapp.net'
-
-// Verifica si es dueño del grupo
-if (user === ownerGroup) 
-return conn.reply(m.chat, 'ꕥ No se puede quitar el rol de administrador al dueño del grupo.', m)
-
-// Verifica si el usuario ya no es admin
-if (!groupInfo.participants.some(p => p.id === user && p.admin))
-return conn.reply(m.chat, 'ꕥ El usuario mencionado ya es miembro normal.', m)
-
-// Ejecuta el demote
+const ownerGroup = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net'
+const ownerBot = global.owner[0][0] + '@s.whatsapp.net'
+if (user === conn.user.jid) return conn.reply(m.chat, `ꕥ No puedes degradar al bot.`, m)
+if (user === ownerGroup) return conn.reply(m.chat, `ꕥ No puedes degradar al creador del grupo.`, m)
+if (user === ownerBot) return conn.reply(m.chat, `ꕥ No puedes degradar al propietario del bot.`, m)
 await conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-await m.react('✅')
-//await conn.reply(m.chat, `❀ Se le quitó el admin correctamente.`, m)
-
+conn.reply(m.chat, `❀ Fue descartado como admin.`, m)
 } catch (e) {
 conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${e.message}`, m)
 }}
 
 handler.help = ['demote']
 handler.tags = ['grupo']
-handler.command = ['demote', 'quitaradmin']
+handler.command = ['demote', 'degradar']
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
