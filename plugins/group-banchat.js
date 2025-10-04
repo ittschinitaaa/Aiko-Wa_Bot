712fbce32431f8b94ed66cbb049533da7d7fe6cd
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-10-04 02:21:15 -0300

    Update group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -1,4 +1,38 @@
 let handler = async (m, { conn, usedPrefix, command, args }) => {
+  let chat = global.db.data.chats[m.chat]
+  let botname = global.botname || "SENKO-BOT 🦊" // <- Definimos el nombre del bot
+
+  if (command === 'bot') {
+    if (args.length === 0) {
+      const estado = chat.isBanned ? '✗ Desactivado' : '✓ Activado'
+      const info = `「✦」Un administrador puede activar o desactivar a *${botname}* utilizando:\n\n✐ _Activar_ » *${usedPrefix}bot on*\n✐ _Desactivar_ » *${usedPrefix}bot off*\n\n✧ Estado actual » *${estado}*`
+      return conn.reply(m.chat, info, m)
+    }
+
+    if (args[0] === 'off' || args[0] === 'disable') {
+      if (chat.isBanned) {
+        return conn.reply(m.chat, `《✦》${botname} ya estaba desactivado.`, m)
+      }
+      chat.isBanned = true
+      return conn.reply(m.chat, `❀ Has *desactivado* a ${botname}!`, m)
+    } else if (args[0] === 'on' || args[0] === 'enable') {
+      if (!chat.isBanned) {
+        return conn.reply(m.chat, `《✦》${botname} ya estaba activado.`, m)
+      }
+      chat.isBanned = false
+      return conn.reply(m.chat, `❀ Has *activado* a ${botname}!`, m)
+    }
+  }
+}
+
+handler.help = ['bot']
+handler.tags = ['grupo']
+handler.command = ['bot']
+handler.admin = true
+
+export default handler
+
+/*let handler = async (m, { conn, usedPrefix, command, args }) => {
 let chat = global.db.data.chats[m.chat]
 if (command === 'bot') {
 if (args.length === 0) {
@@ -25,4 +59,4 @@ handler.tags = ['grupo']
 handler.command = ['bot']
 handler.admin = true
 
-export default handler
+export default handler*/
