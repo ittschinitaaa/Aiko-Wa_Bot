ce415c14083d81afcf4470259c0100f081ca7a56
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-10-11 23:07:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 let { say } = cfonts
 console.log(chalk.magentaBright('\n❀ Iniciando...'))
-say('Senko Bot', {
+say('Draculaura', {
 font: 'simple',
 align: 'left',
 gradient: ['green', 'white']
