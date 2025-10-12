bf8814d6308ad3061e5386b352c124b7f463cd83
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-10-12 12:12:56 -0300

    Update group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -2,7 +2,7 @@ var handler = async (m, { conn, args }) => {
 let group = m.chat
 const pp = await conn.profilePictureUrl(group, 'image').catch((_) => 'https://files.catbox.moe/xr2m6u.jpg')
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-let message = "\`\t*⌁☍꒷₊˚ Link del Grupo ꒷₊˚⌁☍* \`\n\n> \`Link:\` ${link}"
+let message = "\t\` *⌁☍꒷₊˚ Link del Grupo ꒷₊˚⌁☍* \`\n\n> \`Link:\` ${link}"
 await conn.sendMessage(group, { image: { url: pp }, caption: message })
 }
 
