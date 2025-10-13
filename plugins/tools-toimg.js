77c73a3ff0a8ae8817c6cb4148893290b92603c4
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-10-13 01:14:59 -0300

    Update tools-toimg.js

--- plugins/tools-toimg.js
+++ plugins/tools-toimg.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 if (!m.quoted) {
-return conn.reply(m.chat, `❀ Debes citar un sticker para convertir a imagen.`, m)
+return conn.reply(m.chat, `🌸 Debes citar un sticker para convertir a imagen.`, m)
 }
 await m.react('🕒')
 let xx = m.quoted
 let imgBuffer = await xx.download()   
 if (!imgBuffer) {
-await m.react('✖️')
+await m.react('❌')
 return conn.reply(m.chat, `ꕥ No se pudo descargar el sticker.`, m)
 }
-await conn.sendMessage(m.chat, { image: imgBuffer, caption: '❀ *Aquí tienes ฅ^•ﻌ•^ฅ*' }, { quoted: m })
-await m.react('✔️')
+await conn.sendMessage(m.chat, { image: imgBuffer, caption: '🍄 *Aquí tienes ฅ^•ﻌ•^ฅ*' }, { quoted: m })
+await m.react('✅')
 }
 
 handler.help = ['toimg']
