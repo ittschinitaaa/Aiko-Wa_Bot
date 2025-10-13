b707e7ee26fa47708f264c36941b2e43a72a779b
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-10-13 10:37:32 -0300

    Update profile-setmeta.js

--- plugins/profile-setmeta.js
+++ plugins/profile-setmeta.js
@@ -3,7 +3,7 @@ const userId = m.sender
 if (command === 'setmeta') {
 const packParts = text.split(/[\u2022|]/).map(part => part.trim())
 if (packParts.length < 2) {
-return m.reply(`❀ Por favor, escribe el pack y el autor que deseas usar por defecto para tus stickers.\n> Ejemplo: *${usedPrefix + command} Ola • Ola*`)
+return m.reply(`🥞 Por favor, escribe el pack y el autor que deseas usar por defecto para tus stickers.\n> Ejemplo: *${usedPrefix + command} Ola • Ola*`)
 }
 const packText1 = packParts[0]
 const packText2 = packParts[1]
@@ -14,7 +14,7 @@ const packstickers = global.db.data.users[userId]
 packstickers.text1 = packText1
 packstickers.text2 = packText2
 await global.db.write()
-return m.reply(`❀ Se actualizo el pack y autor por defecto para tus stickers.`);
+return m.reply(`🍁 Se actualizo el pack y autor por defecto para tus stickers.`);
 }
 if (command === 'delmeta') {
 if (!global.db.data.users[userId] || (!global.db.data.users[userId].text1 && !global.db.data.users[userId].text2)) {
@@ -24,7 +24,7 @@ const packstickers = global.db.data.users[userId]
 delete packstickers.text1
 delete packstickers.text2
 await global.db.write()
-return m.reply(`❀ Se restablecio el pack y autor por defecto para tus stickers.`)
+return m.reply(`🌺 Se restablecio el pack y autor por defecto para tus stickers.`)
 }}
 
 handler.help = ['setmeta', 'delmeta']
