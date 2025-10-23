0291ab9b5a3a215c3c6af62f699e3d47698e0ee3
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-10-23 12:15:37 -0300

    Update downloads-play.js

--- plugins/downloads-play.js
+++ plugins/downloads-play.js
@@ -28,6 +28,7 @@ if (!text.trim()) return conn.reply(m.chat, `🔔 *Por favor, ingresa el nombre 
      
 *👀 Vistas:* ${vistas}   
 *⏳ Duración:* ${timestamp}
+*💿 Calidad:* 320
 *📆 Publicado:* ${ago}
 *🔗 Link:* ${url}
 
