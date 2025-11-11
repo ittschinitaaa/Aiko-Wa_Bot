4b46dfbafb6a6d171905385546f9ed9cafadbb7d
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-11-10 21:31:22 -0300

    Update group-encuesta.js

--- plugins/group-encuesta.js
+++ plugins/group-encuesta.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
-if (!args[0]) throw `${emoji} Ingrese un texto para iniciar la escuesta.\n\n>📌 Ejemplo : *${usedPrefix + command}* texto|texto2...`
-if (!text.includes('|')) throw  `${emoji2} Separe las encuestas con *|* \n\n> 📌 Ejemplo : *${usedPrefix + command}* texto|texto2...`
+if (!args[0]) throw `🍒 Ingrese un texto para iniciar la escuesta.\n\n>📌 Ejemplo : *${usedPrefix + command}* texto|texto2...`
+if (!text.includes('|')) throw  `🍁 Separe las encuestas con *|* \n\n> 📌 Ejemplo : *${usedPrefix + command}* texto|texto2...`
 let a = []
 let b = text.split('|')
 for (let c = 0; c < b.length; c++) {
@@ -10,7 +10,7 @@ a.push([b[c]])
                         return conn.sendPoll(m.chat, `${packname}`, a, m)
 }
 handler.help = ['encuesta <text|text2>']
-handler.tags = ['grupo'] 
+handler.tags = ['group'] 
 handler.command = ['poll', 'encuesta'] 
 handler.group = true
 
