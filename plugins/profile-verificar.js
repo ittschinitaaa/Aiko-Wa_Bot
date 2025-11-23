ac8519a327d447cc80f4d9b2b697be09d3b9892c
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-11-22 22:35:09 -0300

    Update profile-verificar.js

--- plugins/profile-verificar.js
+++ plugins/profile-verificar.js
@@ -35,15 +35,15 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
 
   let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 20)
 
-  let regbot = `✦ 𝗥 𝗘 𝗚 𝗜 𝗦 𝗧 𝗥 𝗔 𝗗 𝗢 ✦\n`
+  let regbot = `\`⭐ 𝗥 𝗘 𝗚 𝗜 𝗦 𝗧 𝗥 𝗔 𝗗 𝗢 ⭐\`\n`
   regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
   regbot += `> ᰔᩚ Nombre » ${name}\n`
   regbot += `> ✎ Edad » ${age} años\n`
   regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
-  regbot += `❀ 𝗥𝗲𝗰𝗼𝗺𝗽𝗲𝗻𝘀𝗮𝘀:\n`
-  regbot += `> • ⛁ *Monedas* » 40\n`
-  regbot += `> • ✰ *Experiencia* » 300\n`
-  regbot += `> • ❖ *Tokens* » 20\n`
+  regbot += `\`🍒 𝗥𝗲𝗰𝗼𝗺𝗽𝗲𝗻𝘀𝗮𝘀\`:\n`
+  regbot += ` • ⛁ *Monedas* » 40\n`
+  regbot += ` • ✰ *Experiencia* » 300\n`
+  regbot += ` • ❖ *Tokens* » 20\n`
   regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
   regbot += `> ${dev}`
   await m.react('📩')
@@ -72,8 +72,8 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
-        title: '⭐𝗡𝗢𝗧𝗜𝗙𝗜𝗖𝗔𝗖𝗜𝗢𝗡 𝗚𝗘𝗡𝗘𝗥𝗔𝗟 ⭐',
-        body: '🌱 𝐍𝐮𝐞𝐯𝐨 𝐔𝐬𝐮𝐚𝐫𝐢𝐨 𝐫𝐞𝐠𝐢𝐬𝐭𝐫𝐚𝐝𝐨 🌱',
+        title: '【⭐ 𝗡𝗢𝗧𝗜𝗙𝗜𝗖𝗔𝗖𝗜𝗢𝗡 𝗚𝗘𝗡𝗘𝗥𝗔𝗟 ⭐】',
+        body: '\`🌷 𝖭𝗎𝖾𝗏𝗈 𝖴𝗌𝗎𝖺𝗋𝗂𝗈 𝖱𝖾𝗀𝗂𝗌𝗍𝗋𝖺𝖽𝗈 🌷\`',
         thumbnailUrl: pp,
         mediaType: 1,
         renderLargerThumbnail: false
@@ -83,73 +83,7 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
 }
 
 handler.help = ['reg']
-handler.tags = ['rg']
+handler.tags = ['profile']
 handler.command = ['verify', 'verificar', 'reg', 'register', 'registrar']
 
 export default handler
-
-/*import db from '../lib/database.js'
-import fs from 'fs'
-import PhoneNumber from 'awesome-phonenumber'
-import { createHash } from 'crypto'  
-import fetch from 'node-fetch'
-
-let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
-
-let handler = async function (m, { conn, text, usedPrefix, command }) {
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let mentionedJid = [who]
-  let pp = await conn.profilePictureUrl(who, 'image').catch((_) => 'https://files.catbox.moe/xr2m6u.jpg')
-  let user = global.db.data.users[m.sender]
-  let name2 = conn.getName(m.sender)
-  if (user.registered === true) return m.reply(`『✦』Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg*`)
-  if (!Reg.test(text)) return m.reply(`『✦』Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.18*`)
-  let [_, name, splitter, age] = text.match(Reg)
-  if (!name) return m.reply(`『✦』El nombre no puede estar vacío.`)
-  if (!age) return m.reply(`『✦』La edad no puede estar vacía.`)
-  if (name.length >= 100) return m.reply(`『✦』El nombre es demasiado largo.`)
-  age = parseInt(age)
-  if (age > 1000) return m.reply(`『✦』Wow el abuelo quiere jugar al bot.`)
-  if (age < 5) return m.reply(`『✦』hay un abuelo bebé jsjsj.`)
-  user.name = name + '✓'.trim()
-  user.age = age
-  user.regTime = + new Date      
-  user.registered = true
-  global.db.data.users[m.sender].coin += 40
-  global.db.data.users[m.sender].exp += 300
-  global.db.data.users[m.sender].joincount += 20
-  let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 20)
-let regbot = `✦ 𝗥 𝗘 𝗚 𝗜 𝗦 𝗧 𝗥 𝗔 𝗗 𝗢 ✦\n`
-regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
-regbot += `> ᰔᩚ Nombre » ${name}\n`
-regbot += `> ✎ Edad » ${age} años\n`
-regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
-regbot += `❀ 𝗥𝗲𝗰𝗼𝗺𝗽𝗲𝗻𝘀𝗮𝘀:\n`
-regbot += `> • ⛁ *${currency}* » 40\n`
-regbot += `> • ✰ *Experiencia* » 300\n`
-regbot += `> • ❖ *Tokens* » 20\n`
-regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
-regbot += `> ${dev}`
-await m.react('📩')
-
-await conn.sendMessage(m.chat, {
-        text: regbot,
-        contextInfo: {
-            externalAdReply: {
-                title: '🌷 𝗨𝘀𝘂𝗮𝗿𝗶𝗼 𝗩𝗲𝗿𝗶𝗳𝗶𝗰𝗮𝗱𝗼 🌷',
-                body: textbot,
-                thumbnailUrl: pp,
-                sourceUrl: channel,
-                mediaType: 1,
-                showAdAttribution: true,
-                renderLargerThumbnail: true
-            }
-        }
-    }, { quoted: m });    
-}; 
-handler.help = ['reg']
-handler.tags = ['rg']
-handler.command = ['verify', 'verificar', 'reg', 'register', 'registrar'] 
-
-export default handler
-*/
